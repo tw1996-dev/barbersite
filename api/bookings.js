2ec91477a5fa62ced451221ef08da70e69c9dd6b
tw1996-dev <19tomaszwojcik96@gmail.com>
2025-09-17 19:54:23 +0200

    Update bookings.js

--- api/bookings.js
+++ api/bookings.js
@@ -1,3 +1,15 @@
 export default function handler(req, res) {
-  return res.status(200).json({ message: "Function works!" });
+  // testing
+  const hasDB = !!process.env.DATABASE_URL;
+  const hasUnpooled = !!process.env.DATABASE_URL_UNPOOLED;
+  const hasPGHost = !!process.env.PGHOST;
+  
+  return res.status(200).json({ 
+    message: "Function works!",
+    neon_integration: {
+      DATABASE_URL: hasDB,
+      DATABASE_URL_UNPOOLED: hasUnpooled, 
+      PGHOST: hasPGHost
+    }
+  });
 }
