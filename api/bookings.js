a9bbe9b4646524a6eb3d52026eef6fa11139cf7f
tw1996-dev <19tomaszwojcik96@gmail.com>
2025-09-17 19:49:17 +0200

    Update bookings.js

--- api/bookings.js
+++ api/bookings.js
@@ -1,13 +1,3 @@
-module.exports = async function handler(req, res) {
- 
-  const envInfo = {
-    hasDATABASE_URL: !!process.env.DATABASE_URL,
-    hasPGUSER: !!process.env.PGUSER,
-    hasPGPASSWORD: !!process.env.PGPASSWORD,
-    allEnvKeys: Object.keys(process.env).filter(key => 
-      key.includes('PG') || key.includes('DATABASE')
-    )
-  };
-
-  return res.status(200).json(envInfo);
-};
+export default function handler(req, res) {
+  return res.status(200).json({ message: "Function works!" });
+}
